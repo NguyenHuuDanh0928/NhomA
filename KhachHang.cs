1a2b0f41eaf31d08e92eedfda12f0ea917414d8d
Doantantai0802 <doantantaifpt47@gmail.com>
2024-10-05 21:51:18 +0700

    Add files via upload

---
+++ KhachHang.cs
@@ -0,0 +1,15 @@
+﻿namespace KoiVetService
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+        public ICollection<ServiceOrder> ServiceOrders { get; set; }
+    }
+
+}
