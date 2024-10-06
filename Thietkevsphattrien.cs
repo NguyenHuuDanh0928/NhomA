341e5c43d2d54b6bea2f313038ae6ff7b591acfb
PhiTinh49321111 <tinhv3340@gmail.com>
2024-10-06 15:26:12 +0700

    Add files via upload

---
+++ Thietkevsphattrien.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YourNamespace.Models;
+
+namespace YourNamespace.Models;
+    public class ServiceModel
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; } 
+        public string Description { get; set; } 
+        public decimal Price { get; set; } 
+       
+    }
+public class ServicesController : Controller
+{
+    public IActionResult Index()
+    {
+        return View(); 
+    }
+
+    public IActionResult Details(int id)
+    {
+        return View(); 
+    }
+
+    [HttpPost]
+    public IActionResult Book(ServiceModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            return RedirectToAction("Index"); 
+        }
+        return View(model); 
+    }
+}
