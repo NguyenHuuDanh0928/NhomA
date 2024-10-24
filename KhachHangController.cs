f4842f641f1caf53c33165a569a9687701c7eb4c
HoaiNam <namvohoai26@gmail.com>
2024-10-24 22:58:31 +0700

    Add files via upload
    Thông tin khách hàng đăng ký

--- KhachHangController.cs
+++ KhachHangController.cs
@@ -1,106 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using KoiFishVetClinicAPI.Data;
-using KoiFishVetClinicAPI.Models;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WebDichVu.DuLieu;
+using WebDichVu.Helpers;
+using WebDichVu.ViewModel;
 
-namespace KoiFishVetClinicAPI.Controllers
+namespace WebDichVu.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class KhachHangController : ControllerBase
+    public class KhachHangController : Controller
     {
-        private readonly KoiFishVetClinicDbContext _context;
+        private readonly PetShopDbContext db;
+        private readonly IMapper _mapper;
 
-        public KhachHangController(KoiFishVetClinicDbContext context)
+        public KhachHangController(PetShopDbContext context, IMapper mapper)
+
         {
-            _context = context;
-        }
+            db = context;
+            _mapper = mapper;
 
-        // GET: api/KhachHang
+        }
         [HttpGet]
-        [Produces("application/json")]
-        public async Task<ActionResult<IEnumerable<KhachHang>>> GetKhachHangs()
+        public IActionResult DangKy()
         {
-            return await _context.KhachHang.ToListAsync();
+            return View();
         }
-
-        // GET: api/KhachHang/5
-        [HttpGet("{id}")]
-        [Produces("application/json")]
-        public async Task<ActionResult<KhachHang>> GetKhachHang(int id)
-        {
-            var khachHang = await _context.KhachHang.FindAsync(id);
-
-            if (khachHang == null)
-            {
-                return NotFound();
-            }
-
-            return khachHang;
-        }
-
-        // POST: api/KhachHang
         [HttpPost]
-        [Produces("application/json")] 
-        public async Task<ActionResult<KhachHang>> PostKhachHang(KhachHang khachHang)
+        public IActionResult DangKy(RegisterVm model)
         {
-            _context.KhachHang.Add(khachHang);
-            await _context.SaveChangesAsync();
+            if (ModelState.IsValid)
+            {
+                var khachHang = _mapper.Map<KhachHang>(model);
+                khachHang.RandomKey = Util.GenerateRamdomKey();
+                khachHang.Matkhau = model.MatKhau.ToMd5Hash(khachHang.RandomKey);
 
-            return CreatedAtAction("GetKhachHang", new { id = khachHang.Id }, khachHang);
+            }
+            return View();
+        }
+        #region Login
+        [HttpGet]
+        public IActionResult DangNhap(string? ReturnUrl)
+        {
+            ViewBag.ReturnUrl = ReturnUrl;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DangNhap(LoginVM model, string? ReturnUrl)
+        {
+
+            ViewBag.ReturnUrl = ReturnUrl;
+            return View();
         }
 
-        // PUT: api/KhachHang/5
-        [HttpPut("{id}")]
-        [Produces("application/json")]
-        public async Task<IActionResult> PutKhachHang(int id, KhachHang khachHang)
-        {
-            if (id != khachHang.Id)
-            {
-                return BadRequest();
-            }
 
-            _context.Entry(khachHang).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!KhachHangExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // DELETE: api/KhachHang/5
-        [HttpDelete("{id}")]
-        [Produces("application/json")]
-        public async Task<IActionResult> DeleteKhachHang(int id)
-        {
-            var khachHang = await _context.KhachHang.FindAsync(id);
-            if (khachHang == null)
-            {
-                return NotFound();
-            }
-
-            _context.KhachHang.Remove(khachHang);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool KhachHangExists(int id)
-        {
-            return _context.KhachHang.Any(e => e.Id == id);
-        }
+        #endregion
     }
 }
