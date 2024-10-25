62cdf2a4c5574c457ab88bd78c7c4daaa100dcf1
Tai0000000 <ngoviettai1010@gmail.com>
2024-10-25 19:22:41 +0700

    KhachHangAPI.cs

---
+++ KhachHangAPI.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebDichVu.DuLieu;
+
+namespace WebDichVu.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KhachHangAPI : ControllerBase
+    {
+        private readonly PetShopDbContext _context;
+
+        public KhachHangAPI(PetShopDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/KhachHang
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<KhachHang>>> GetKhachHangs()
+        {
+            return await _context.KhachHangs.ToListAsync();
+        }
+
+        // GET: api/KhachHang/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<KhachHang>> GetKhachHang(string id)
+        {
+            var khachHang = await _context.KhachHangs.FindAsync(id);
+
+            if (khachHang == null)
+            {
+                return NotFound();
+            }
+
+            return khachHang;
+        }
+
+        // PUT: api/KhachHang/5
+       
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutKhachHang(string id, KhachHang khachHang)
+        {
+            if (id != khachHang.MaKh)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(khachHang).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!KhachHangExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/KhachHang
+        
+        [HttpPost]
+        public async Task<ActionResult<KhachHang>> PostKhachHang(KhachHang khachHang)
+        {
+            _context.KhachHangs.Add(khachHang);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (KhachHangExists(khachHang.MaKh))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetKhachHang", new { id = khachHang.MaKh }, khachHang);
+        }
+
+        // DELETE: api/KhachHang/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteKhachHang(string id)
+        {
+            var khachHang = await _context.KhachHangs.FindAsync(id);
+            if (khachHang == null)
+            {
+                return NotFound();
+            }
+
+            _context.KhachHangs.Remove(khachHang);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool KhachHangExists(string id)
+        {
+            return _context.KhachHangs.Any(e => e.MaKh == id);
+        }
+    }
+}
