24147fd515c72099ea777e2948749c962cac2632
Tai0000000 <ngoviettai1010@gmail.com>
2024-10-25 19:20:50 +0700

    LichSuDatDichVuAPI.cs

--- LichSuDatDichVuAPI.cs
+++ LichSuDatDichVuAPI.cs
@@ -1,35 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using KoiFishVetClinicAPI.Data;
-using KoiFishVetClinicAPI.Models;
+using WebDichVu.DuLieu;
 
-namespace KoiFishVetClinicAPI.Controllers
+namespace WebDichVu.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class LichSuDatDichVuController : ControllerBase
+    public class LichSuDatDichVuAPI : ControllerBase
     {
-        private readonly KoiFishVetClinicDbContext _context;
+        private readonly PetShopDbContext _context;
 
-        public LichSuDatDichVuController(KoiFishVetClinicDbContext context)
+        public LichSuDatDichVuAPI(PetShopDbContext context)
         {
             _context = context;
         }
 
         // GET: api/LichSuDatDichVu
         [HttpGet]
-        [Produces("application/json")]
         public async Task<ActionResult<IEnumerable<LichSuDatDichVu>>> GetLichSuDatDichVus()
         {
-            return await _context.LichSuDatDichVu.ToListAsync();
+            return await _context.LichSuDatDichVus
+                .Include(l => l.DichVu)
+                .Include(l => l.KhachHang)
+                .ToListAsync();
         }
 
         // GET: api/LichSuDatDichVu/5
         [HttpGet("{id}")]
-        [Produces("application/json")]
         public async Task<ActionResult<LichSuDatDichVu>> GetLichSuDatDichVu(int id)
         {
-            var lichSuDatDichVu = await _context.LichSuDatDichVu.FindAsync(id);
+            var lichSuDatDichVu = await _context.LichSuDatDichVus
+                .Include(l => l.DichVu)
+                .Include(l => l.KhachHang)
+                .FirstOrDefaultAsync(l => l.Id == id);
 
             if (lichSuDatDichVu == null)
             {
@@ -39,21 +42,10 @@ namespace KoiFishVetClinicAPI.Controllers
             return lichSuDatDichVu;
         }
 
-        // POST: api/LichSuDatDichVu
-        [HttpPost]
-        [Produces("application/json")]
-        public async Task<ActionResult<LichSuDatDichVu>> PostLichSuDatDichVu(LichSuDatDichVu lichSuDatDichVu)
-        {
-            _context.LichSuDatDichVu.Add(lichSuDatDichVu);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetLichSuDatDichVu", new { id = lichSuDatDichVu.Id }, lichSuDatDichVu);
-        }
-
         // PUT: api/LichSuDatDichVu/5
+        
         [HttpPut("{id}")]
-        [Produces("application/json")]
-        public async Task<IActionResult> PutLichSuDatDichVu(int id, LichSuDatDichVu lichSuDatDichVu)
+        public async Task<IActionResult> PutLichSuDatDichVu(int id, [Bind("KhachHangId, DichVuId, ThoiGian, TrangThai")] LichSuDatDichVu lichSuDatDichVu)
         {
             if (id != lichSuDatDichVu.Id)
             {
@@ -81,18 +73,28 @@ namespace KoiFishVetClinicAPI.Controllers
             return NoContent();
         }
 
+        // POST: api/LichSuDatDichVu
+        
+        [HttpPost]
+        public async Task<ActionResult<LichSuDatDichVu>> PostLichSuDatDichVu([Bind("KhachHangId, DichVuId, ThoiGian, TrangThai")] LichSuDatDichVu lichSuDatDichVu)
+        {
+            _context.LichSuDatDichVus.Add(lichSuDatDichVu);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLichSuDatDichVu", new { id = lichSuDatDichVu.Id }, lichSuDatDichVu);
+        }
+
         // DELETE: api/LichSuDatDichVu/5
         [HttpDelete("{id}")]
-         [Produces("application/json")] 
         public async Task<IActionResult> DeleteLichSuDatDichVu(int id)
         {
-            var lichSuDatDichVu = await _context.LichSuDatDichVu.FindAsync(id);
+            var lichSuDatDichVu = await _context.LichSuDatDichVus.FindAsync(id);
             if (lichSuDatDichVu == null)
             {
                 return NotFound();
             }
 
-            _context.LichSuDatDichVu.Remove(lichSuDatDichVu);
+            _context.LichSuDatDichVus.Remove(lichSuDatDichVu);
             await _context.SaveChangesAsync();
 
             return NoContent();
@@ -100,7 +102,7 @@ namespace KoiFishVetClinicAPI.Controllers
 
         private bool LichSuDatDichVuExists(int id)
         {
-            return _context.LichSuDatDichVu.Any(e => e.Id == id);
+            return _context.LichSuDatDichVus.Any(e => e.Id == id);
         }
     }
 }
