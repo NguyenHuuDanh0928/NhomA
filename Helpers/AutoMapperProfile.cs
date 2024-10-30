f788214ccad89630103096ea065c937e6e157c00
HoaiNam <namvohoai26@gmail.com>
2024-10-30 14:06:33 +0700

    Helpers
    Tạo folder Helpers và add 4 file này vào để thực hiện hỗ trợ phần đăng ký và đăng nhập

---
+++ Helpers/AutoMapperProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using WebDichVu.Datas;
+using WebDichVu.ViewModel;
+
+namespace WebDichVu.Helpers
+{
+	public class AutoMapperProfile : Profile
+	{
+		public AutoMapperProfile()
+		{
+			CreateMap<RegisterVm, KhachHang>()
+				.ForMember(dest => dest.TenKhachHang, opt => opt.MapFrom(src => src.HoTen))
+				.ForMember(dest => dest.DiaChi, opt => opt.MapFrom(src => src.DiaChi))
+				.ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+				.ForMember(dest => dest.SoDienThoai, opt => opt.MapFrom(src => src.SoDienThoai))
+				.ForMember(dest => dest.NgaySinh, opt => opt.MapFrom(src => DateOnly.FromDateTime(src.NgaySinh)))
+				.ForMember(dest => dest.GioiTinh, opt => opt.MapFrom(src => src.GioiTinh == RegisterVm.GioiTinhEnum.Nam));
+		}
+	}
+
+}
