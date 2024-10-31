ed9066c474666fb723dd7f333c43f5133994886c
HoaiNam <namvohoai26@gmail.com>
2024-10-31 21:22:04 +0700

    Vị trí các file như sau:
    Controller/AdminLoginController, Shared/LoginLayout , Models/AdminAuthorizeAttribute , AdminLogin/Login.cshtml. nếu không có các Folder thì tạo mới đảm bảo nằm trong Admin

---
+++ AdminLoginController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebDichVu.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminLoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            if (username == "admin" && password == "password123") // Thay thế bằng tài khoản và mật khẩu của bạn
+            {
+                HttpContext.Session.SetString("AdminLoggedIn", "true");
+                return RedirectToAction("Index", "HomeAdmin");
+            }
+
+            ViewBag.Error = "Tên đăng nhập hoặc mật khẩu không đúng";
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("AdminLoggedIn");
+            return RedirectToAction("Login");
+        }
+    }
+}
