65a0bc0932bd0d98829af70441a1ec4e97404118
PhiTinh49321111 <tinhv3340@gmail.com>
2024-11-16 17:16:20 +0700

    Delete Phi Tịnh directory

--- Phi Tịnh/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebDichVu.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
